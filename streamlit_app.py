058e06e4ea7f2598cb24dd1d09092ae4065ae638
Snowflake20249 <159447503+Snowflake20249@users.noreply.github.com>
2024-02-14 12:47:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,40 +3,30 @@ import snowflake.connector
 import pandas as pd
 import requests as rq
 
-#from frosty import parse_sentence
-#import snowpark
-#from snowpark.sql import SparkSession
-
+def func_return_query(vStr):
+    return vStr
 
 st.title("Hello - Please ask question to Snowflake Database")
+st.text("Sample - select * from snowflake_sample_data.tpch_sf1.lineitem limit 5")
 question = st.text_input("Ask your question")
-
+st.text(question)
 
 
 if st.button("Ask"):
-    # Parse user question
-    st.text(question)
+    
     # API to convert Natural language to SQL
-    vSQL= question
-        
+    st.text("Convert to")
+    vSQL= func_return_query(question)
+    st.text(vSQL)    
+    
+    # Execute SQL
     my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
     my_cur = my_cnx.cursor()
     my_cur.execute(vSQL)   # "SELECT * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST"
     my_data_row = my_cur.fetchall()
 
     load_df = pd.DataFrame(my_data_row)
-    #fruit_load_df.rename(columns = {0: 'Fruit Name'},inplace = True)
-    #st.header('Fruit Load Contains')
-    #st.dataframe(load_df)
     st.dataframe(load_df)
 
-    #parsed_query = parse_sentence(question)
-    # Convert parsed query to Snowpark compatible SQL
-    #snowpark_query = parsed_query.to_sql()
-    # Execute query in Snowflake
-    #result = spark.sql(snowpark_query)
-    # Display results
-    # st.write(result.toPandas())
     my_cnx.close()
-
-
+    
