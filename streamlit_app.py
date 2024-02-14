e36ca0a782e74f36845b174bd11378026a2fe82c
Snowflake20249 <159447503+Snowflake20249@users.noreply.github.com>
2024-02-14 14:17:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ question = st.text_input("Ask your question")
 
 
 if st.button("Ask"):    
+    st.spinner("Thinking)
     # API to convert Natural language to SQL
     st.text(question)
     st.text("Convert to")
