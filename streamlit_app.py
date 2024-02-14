92c087aa1ffa8d128fea1346b7d7b4675abc27f2
Snowflake20249 <159447503+Snowflake20249@users.noreply.github.com>
2024-02-14 14:20:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,14 @@ if st.button("Ask"):
     st.text(vSQL)    
     
     # Execute SQL
-    st.spinner("Thinking"):
-        my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-        my_cur = my_cnx.cursor()
-        my_cur.execute(vSQL)   # "SELECT * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST"
-        my_data_row = my_cur.fetchall()
     
-        load_df = pd.DataFrame(my_data_row)
-        st.dataframe(load_df)
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+    my_cur = my_cnx.cursor()
+    my_cur.execute(vSQL)   # "SELECT * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST"
+    my_data_row = my_cur.fetchall()
+
+    load_df = pd.DataFrame(my_data_row)
+    st.dataframe(load_df)
 
     my_cnx.close()
     
